e097ec49016e4966235d33cfc44eb5a55e90256e
Jameel Hassan <jhassan.tck14@gmail.com>
2023-03-01 20:02:58 +0400

    copied TPT

---
+++ scripts/test_tpt_coop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+data_root='./datasets'
+coop_weight='/path/to/pretrained/coop/weight.pth'
+testsets=$1
+arch=RN50
+# arch=ViT-B/16
+bs=64
+
+python ./tpt_classification.py ${data_root} --test_sets ${testsets} \
+-a ${arch} -b ${bs} --gpu 0 \
+--tpt 
+# --load ${coop_weight}

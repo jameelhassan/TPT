67e310562ad72084e507116384f89d819687b422
Jameel Hassan <jhassan.tck14@gmail.com>
2023-03-12 19:41:01 +0400

    minor update

--- scripts/test_tpt_maple.sh
+++ scripts/test_tpt_maple.sh
@@ -8,11 +8,6 @@ arch=ViT-B/16
 bs=64
 
 python ./tpt_classification.py ${data_root} --test_sets ${testsets} \
-<<<<<<< HEAD
--a ${arch} -b ${bs} --gpu 1 --n_ctx 2 \
---tpt --maple --load ${coop_weight}
-=======
 -a ${arch} -b ${bs} --gpu 0 --n_ctx 2 --maple_depth 3 \
 --tpt --maple \
 --load ${maple_weight}
->>>>>>> 937d2544605f3f7a5db4b789b2f5234257b49c83
